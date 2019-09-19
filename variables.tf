4b9c2a3493a79b1c3d2e81c7a1c6e93ae3c982db
KavinAbelak <kavin.pl@gmail.com>
2019-09-19 12:08:03 +0100

    make associate public ip a bool and default to false

--- variables.tf
+++ variables.tf
@@ -272,8 +272,7 @@ variable "db_subnet_group_name" {
 
 variable "associate_public_ips" {
   description = "Assign public IPs to airflow EC-2 instances, if false machine where terraform is run will need access to the VPC via VPN, tunneling etc."
-  type = "string"
-  default = true
+  default = false
 }
 
 #------------------------------------------------------------
